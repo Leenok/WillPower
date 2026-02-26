f1606c19e519e8775965abdebb3a8a8b5c7fd93b
elena.seredich <eseredich8@gmail.com>
2026-02-27 01:36:25 +0300

    feat: add login and logout

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('', views.home, name='home'),
+    path('workout_list/', views.workout_list, name='workout_list'),
+    # Все маршруты аутентификации Django
+    path('register/', views.register, name='register'),
+    # path('login/', views.login, name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Workout, Exercise, WorkoutExercise
 
-# Register your models here.
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'date', 'workout_type', 'duration')
+    list_filter = ('workout_type', 'date', 'user')
+    search_fields = ('title', 'notes')
+    date_hierarchy = 'date'
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'muscle_group')
+    list_filter = ('muscle_group',)
+    search_fields = ('name',)
+
+@admin.register(WorkoutExercise)
+class WorkoutExerciseAdmin(admin.ModelAdmin):
+    list_display = ('workout', 'exercise', 'sets', 'reps', 'weight')
+    list_filter = ('workout__user',)
+
